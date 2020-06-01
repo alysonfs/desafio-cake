59aae36489045528d8c5e892eba9d2e0412871c9
Alyson F Souza <alysonforever@gmail.com>
2020-06-01 17:23:12 -0300

    feat: configurado acesso banco de dados local

--- config/app_local.php
+++ config/app_local.php
@@ -36,27 +36,11 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
-            /*
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'non_standard_port_number',
-
-            'username' => 'my_app',
-            'password' => 'secret',
-
-            'database' => 'my_app',
-            /**
-             * If not using the default 'public' schema with the PostgreSQL driver
-             * set it here.
-             */
-            //'schema' => 'myapp',
-
-            /**
-             * You can use a DSN string to set the entire configuration
-             */
+            'host' => 'localhost',          
+            'port' => 3306,
+            'username' => 'root',
+            'password' => '',
+            'database' => 'desafio',
             'url' => env('DATABASE_URL', null),
         ],
 
